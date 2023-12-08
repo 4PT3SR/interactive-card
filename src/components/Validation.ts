3262ef86dc0fedc8cf32d8130bd58cca047fcc9e
apt3sr <kingtoluene@gmail.com>
2023-12-08 10:10:12 +0100

    adjusted cardholder name regex pattern

--- src/components/Validation.ts
+++ src/components/Validation.ts
@@ -5,7 +5,7 @@ const validYear = (value:number) => {
     return value >= currentYear 
   };
 export const validationSchema = Yup.object().shape({
-    name:Yup.string().required('Required!').min(3,'too short!').matches(/^[A-Za-z]+$/,'Only alphabets!'),
+    name:Yup.string().required('Required!').min(3,'too short!').matches(/^[a-zA-Z ]+$/,'Only alphabets!'),
     cardNumber:Yup.string().required("Can't be blank!").min(16,'Atleast 16 digits').max(16,'At most 16 digits'),
     month:Yup.number().required("Can't be blank!").max(12,'not a valid month').min(1,'not a valid month'),
     year:Yup.number().required("Can't be blank!").test('is-valid-year','Not a valid Year',validYear),
